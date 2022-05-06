6ddef8d4851574cc29a06340920fdadabd57efad
andre <andre@egend.com.br>
2022-05-06 11:53:28 -0300

    remove coverage

--- src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -2,11 +2,11 @@ import {MailAdapter, SendMailData} from "../mail-adapter";
 import nodemailer from "nodemailer";
 
 const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
+    host: "",
     port: 2525,
     auth: {
-        user: "08bd90903b55d7",
-        pass: "37ed4d1369d9a5"
+        user: "",
+        pass: ""
     }
 });
 
